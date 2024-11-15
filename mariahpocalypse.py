6d86a5a134d4bbf427513b902fe6a4a1ac838227
girlwholeft <136718160+girlwholeft@users.noreply.github.com>
2024-11-15 12:56:25 -0500

    Add files via upload

---
+++ mariahpocalypse.py
@@ -0,0 +1,28 @@
+from pygame import mixer
+import random
+import time
+
+files = ["file1.mp3",
+"file2.mp3",
+"file3.mp3",
+"file5.mp3",
+"file7.mp3",
+"file8.mp3",
+"file11.mp3"]
+
+def countdown_mariah(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs) 
+        time.sleep(1) 
+        t -= 1
+
+    song = random.choice(files)
+    mixer.init()
+    mixer.music.load(song)
+    mixer.music.play()
+
+seconds = random.randint(1, 3)
+
+while True:
+    countdown_mariah(seconds)
