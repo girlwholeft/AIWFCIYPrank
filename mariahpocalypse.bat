c6de6ecf46d31923e813d66d2da2ba31c78fa94a
girlwholeft <136718160+girlwholeft@users.noreply.github.com>
2024-11-15 12:58:37 -0500

    mariahpocalypse.bat

--- mariahpocalypse.bat
+++ mariahpocalypse.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCuDJE2Q5EYgKQtRXkqLP2Ta
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSTk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCuDJE2Q5EYgKQtRXlXRbTjrVeRPubuqvKSCukh9
-::YB416Ek+ZW8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 Powershell -Command "Invoke-WebRequest -Uri 'https://www.dropbox.com/scl/fi/n1gpikb87fvwsv3bx4uc2/ttpdpaper.jpg?rlkey=upe3ao99ydyyyu0ogeza08b1k&st=j7xskure&dl=1' -OutFile .\ttpdpaper.jpg"
 start ttpdpaper.jpg
 cd %TEMP%
